53e2ad057893f9d913b34178879175ef9bcdb857
Quincy So <code.soq@soquincy.qzz.io>
2026-05-25 19:43:10 +0800

    change model in search.py

--- utils/search.py
+++ utils/search.py
@@ -24,7 +24,7 @@ SEARCH_ENGINE_ID      = os.getenv("SEARCH_ENGINE_ID")
 # ---------------------------------------------------------------------------
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-SEARCH_MODEL   = "gemini-2.0-flash"
+SEARCH_MODEL   = "gemini-flash-lite-latest"
 # ---------------------------------------------------------------------------
 # Result type
 # ---------------------------------------------------------------------------
