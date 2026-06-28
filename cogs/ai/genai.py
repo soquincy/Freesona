911b51079a8f9e45170bac582db5002360896b1a
Quincy So <code.soq@soquincy.qzz.io>
2026-06-28 19:39:04 +0800

    readded reply_to=message on autonoomy response

--- cogs/ai/genai.py
+++ cogs/ai/genai.py
@@ -257,7 +257,7 @@ class GenAICog(commands.Cog):
                         username=message.author.display_name,
                         attachments=attachments,
                     )
-                    await send_response(response, message.channel)
+                    await send_response(response, message.channel, reply_to=message)
 
     # -------------------------------------------------------------------
     # ~write
